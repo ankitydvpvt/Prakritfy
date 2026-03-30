ce13a4dc5e4f7dbb256113dd3c72fa9a06b9dcd7
Ankitydv3 <ankitydvpvt@gmail.com>
2026-03-30 14:24:35 +0530

    PatentFromUI

--- Nutribot_backend-main/src/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { chatWithAI } = require("../controllers/chatController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.post("/", chatWithAI);
-
-module.exports = router;
